8f7ca7c4c5ab00dd77e18536f3f47b16a4c13f09
namsuhan0322 <rnatpakfl@gmail.com>
2026-04-09 18:15:43 +0900

    [Feat] 애니메이션 동기화, 점프, 카메라 권한 확인

--- Assets/Scripts/FusionBootstrap.cs
+++ Assets/Scripts/FusionBootstrap.cs
@@ -27,6 +27,7 @@ public class FusionBootstrap : MonoBehaviour, INetworkRunnerCallbacks
     public enum InputButton
     {
         Fire = 0,
+        Jump = 1,
     }
 
     public void StartHost() => _ = StartGame(GameMode.Host);
@@ -69,7 +70,7 @@ public class FusionBootstrap : MonoBehaviour, INetworkRunnerCallbacks
     {
         Debug.Log($"플레이어 입장 : {player}");
 
-        if (runner.IsPlayer == false) return;
+        if (!runner.IsServer) return;
 
         Vector3 spawnPos = GetSpawnPosition(player);
 
@@ -105,7 +106,8 @@ public class FusionBootstrap : MonoBehaviour, INetworkRunnerCallbacks
         );
 
         var buttons = new NetworkButtons();                                 // 네트워크 버튼 생성
-        buttons.Set((int)InputButton.Fire, Input.GetMouseButton(0));    // 마우스 버튼
+        buttons.Set((int)InputButton.Fire, Input.GetMouseButton(0));        // 마우스 버튼
+        buttons.Set((int)InputButton.Jump, Input.GetKey(KeyCode.Space));    // 점프 버튼 
 
         data.buttons = buttons;
 
